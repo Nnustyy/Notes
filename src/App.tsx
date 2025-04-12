888d416e4c72e67deefabe8d9327a3a3ed5de6f6
Anastasia <anastasiaerofeeva121@gmail.com>
2025-04-12 14:47:53 +0300

    first note

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { FC, use, useState } from 'react';
+import './styles/index.scss'
+const App:FC = () => {
 
-const App = () => {
+
   return (
-    <div>
-      
+    <div  className='App'>
+      <div className="note">
+        <div className="note-content">
+          <div className='note-title' >1 Title</div>
+          <div className='note-desc'>
+            descripton
+          </div>
+        </div>
+        <div className="note-btns">
+          <button className='note-delete-btn' >Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Components/Note.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+interface NoteItemProps {
+  
+}
+
+const NoteItem:FC<NoteItemProps> = () => {
+  return (
+    <div>
+      <h1>Note item</h1>
+    </div>
+  );
+};
+
+export default NoteItem;
