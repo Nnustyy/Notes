3932557602a0f6fe5539499f181552ad31a73c4f
Anastasia <anastasiaerofeeva121@gmail.com>
2025-05-07 02:25:24 -0700

    bin restore

--- src/Components/BinItem.tsx
+++ src/Components/BinItem.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { INote } from './context/NoteContext';
+import React, { useContext } from 'react';
+import { INote, NoteContext } from './context/NoteContext';
 import MyButton from './UI/MyButton/MyButton';
 import classes from './UI/MyButton/MyButton.module.scss'
 
 
+
 const BinItem = (deletedNote:INote) => {
+
+  const {recentlyDeletedNotes,setRecentlyDeletedNotes,setNotes} = useContext(NoteContext)
+
+  const restore = ((deletedNote:INote) => {
+  const updatedNotes = (recentlyDeletedNotes.filter((note) => note.id !== deletedNote.id))
+  setRecentlyDeletedNotes(updatedNotes)
+  setNotes((prev => [...prev, deletedNote]))
+
+  
+  })
+
   return (
     <div className='bin'>
       <div className="bin-content">
         <div className="bin-title">{deletedNote.title}</div>
         <div className="bin-desc">{deletedNote.desc}</div>
         <div className="bin-actions">
-          <MyButton className={classes.buttonRestore} >Restore</MyButton>
+          <MyButton onClick={() => restore(deletedNote)} className={classes.buttonRestore} >Restore</MyButton>
           <MyButton className={classes.buttonDelete} >Delete</MyButton>
         </div>
       </div>
